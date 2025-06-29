9c267748ed5cad6ac7af8b2381744b9d87d02cfa
Lorelei <145348413+Shikein-Lorelei@users.noreply.github.com>
2025-06-29 12:51:23 +0700

    First

--- src/components/BackTop.jsx
+++ src/components/BackTop.jsx
@@ -23,7 +23,7 @@ export default function BackTop() {
         onClick={() =>
           document.getElementById("home").scrollIntoView({ behavior: "smooth" })
         }
-        className="fixed bottom-4 right-4 px-2 py-2 z-50 bg-transparent text-white font-bold text-lg transform -skew-x-12 transition-all duration-200 border-2 border-red-600 shadow-lg hover:bg-red-600 hover:shadow-2xl hover:shadow-red-600/50 hover:scale-105 active:scale-95 overflow-hidden"
+        className="fixed bottom-4 right-4 px-2 py-2 z-50 bg-red-700 text-white font-bold text-lg transform -skew-x-12 transition-all duration-200 border-2 border-red-600 hover:bg-red-600  hover:scale-105 active:scale-95 overflow-hidden"
       >
         <ArrowBigUp className="w-8 h-8"/>
       </button>
